6e67b9ffcd7e5f224aed809d5dabe88840045a6a
MUHAMMED BURAK ALTUN <MALTUN15@ku.edu.tr>
2021-12-20 10:28:37 +0300

    Redux Store Configurations

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
 
 class CategoryList extends Component {
     render() {
         return (
             <div>
                 <h1> Category List </h1>
+                <h6> {this.props.currentCategory.name} </h6>
             </div>
         );
     }
 }
 
-export default CategoryList;
+function mapStateToProps(state) {
+    return {
+        currentCategory: state.changeCategoryReducer
+    }
+}
+
+export default connect(mapStateToProps)(CategoryList);
