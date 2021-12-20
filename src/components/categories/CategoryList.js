5c48d1452a447a26986f06b0b501db874df12e72
MUHAMMED BURAK ALTUN <MALTUN15@ku.edu.tr>
2021-12-20 10:28:57 +0300

    Redux Store Configurations

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -7,6 +7,7 @@ class CategoryList extends Component {
             <div>
                 <h1> Category List </h1>
                 <h6> {this.props.currentCategory.name} </h6>
+                <h5> {this.props.currentCategory.name} </h5>
             </div>
         );
     }
